acd152f958fd6533189d9e895d2e8b34b93ef36c
Saptarshi Talukdar <146040609+SattiDaBeaver@users.noreply.github.com>
2024-10-28 23:59:19 -0400

    Add files via upload
    28 Oct 2024

---
+++ Globals.h
@@ -0,0 +1,17 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+
+#define DINO_HEIGHT 22
+#define DINO_WIDTH 22
+
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+
+#define BUTTON 4
+
+#define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+
+#endif
